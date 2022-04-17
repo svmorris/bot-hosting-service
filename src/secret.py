593cbae5339706b5c44746f33e5c7ade7b34463d
svmorris <svmorris@protonmail.com>
2022-04-17 19:37:38 +0200

    Remove default admin password and password autofill on login page

--- src/secret.py
+++ src/secret.py
@@ -1,6 +1,6 @@
 user_creds = [
         {
             "username": "admin",
-            "password": "$2b$08$d7nZnXCWUhvEUofbw6tWS.pKv4iKHQhi/gmMgBLOBfQSMrPmPfXCa"
+            "password": ""
             }
         ]
